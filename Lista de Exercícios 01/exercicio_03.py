84071929014b115bdad2b2a17ce19d0c2472baa6
Lay Cipriano <88255197+LayCipriano@users.noreply.github.com>
2024-07-25 19:13:48 -0300

    exercício 3 finalizado

--- Lista de Exercícios 01/exercicio_03.py
+++ Lista de Exercícios 01/exercicio_03.py
@@ -1,2 +1,13 @@
 '''Faça um Programa que peça a quantidade de quilômetros, transforme
 em metros, centímetros e milímetros'''
+
+qntKm = float(input("Informe a quilometragem: "))
+
+kmMetro = qntKm * 1000
+kmCentimetro = kmMetro * 100
+kmMilimetro = kmCentimetro * 10
+
+print(f"O valor de {qntKm}Km equivale a:")
+print(f"uma distância de {kmMetro:,}m (metros)")
+print(f"uma distância de {kmCentimetro:,}cm (centímetros)")
+print(f"uma distância de {kmMilimetro:,}mm (milímetros)")
