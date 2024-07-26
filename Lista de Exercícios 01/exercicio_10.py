87aa646c839cf95ed9c821f5a047435f057113a5
Lay Cipriano <88255197+LayCipriano@users.noreply.github.com>
2024-07-26 16:50:37 -0300

    exercicio 10 lista 01 - done

---
+++ Lista de Exercícios 01/exercicio_10.py
@@ -0,0 +1,16 @@
+'''Faça um Programa que utilize 4 variáveis como preferir e no final print
+uma mensagem amigável utilizando as variáveis criadas.
+Exemplos de variáveis: nome, idade, lugar, profissão ....
+Exemplo de retorno: Olá Maria, prazer te conhecer. Sou de São Paulo
+também e estou migrando de área.
+'''
+
+nome = 'Laysa'
+idade = 25
+linguagem = 'Python'
+meta = 'contribuir para um mundo melhor'
+
+print(f'''
+      Olá, meu nome é {nome}! Eu tenho {idade} anos e trabalho na área de TI.
+      Meu objetivo é aprender uma nova linguagem chamada {linguagem} e {meta} por meio da tecnologia!
+      ''')
