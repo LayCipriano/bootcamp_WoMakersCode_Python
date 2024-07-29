c239d4216aae78f3217ad499853bd8b2a1b001c5
Lay Cipriano <88255197+LayCipriano@users.noreply.github.com>
2024-07-29 19:15:19 -0300

    EX01-LISTA02-DONE

---
+++ Lista de Exercícios 02/exercicio_01.py
@@ -0,0 +1,13 @@
+# Exercícios Tomada de Decisão
+# Faça um Programa que peça dois números e imprima o maior deles.
+
+print(f"______ DESCUBRA O MAIOR ______")
+num1 = int(input("Informe o 1º número: "))
+num2 = int(input("Informe o 2º número: "))
+
+if num1 > num2: 
+    print(f"O número {num1} é maior que o número {num2}!")
+else:
+    print(f"O número {num2} é maior que o número {num1}!")
+    
+    
