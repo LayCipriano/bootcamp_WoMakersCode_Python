4d4c16cb136803fe2a1c660d2d17805782a44e43
Lay Cipriano <88255197+LayCipriano@users.noreply.github.com>
2024-07-29 22:05:39 -0300

    EX01-LISTA02-DONE

---
+++ Lista de Exercícios 02/exercicio_05.py
@@ -0,0 +1,17 @@
+# Exercícios Tomada de Decisão
+'''Desenvolva um programa que solicite ao usuário os comprimentos dos três
+lados de um triângulo e classifique-o como equilátero, isósceles ou escaleno.
+equilátero: todos os lados com o mesmo valor
+isósceles: dois lados com o mesmo valor
+escaleno: todos os lados com medidas distintas'''
+
+left = float(input("Informe as medidas do lado esquerdo do triângulo: "))
+right = float(input("Informe as medidas do lado esquerdo do triângulo: "))
+bottom = float(input("Informe as medidas da base do triângulo: "))
+
+if left == right == bottom:
+    print("Triângulo Equilátero: todos os lados com o mesmo valor")
+elif left == right or left == bottom or right == bottom:
+    print("Triângulo Isósceles: dois lados com o mesmo valor")
+else:
+    print("Triângulo Escaleno: todos os lados com medidas distintas")
