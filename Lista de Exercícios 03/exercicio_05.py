e5cec7cb3d74a4484a49d724ff5b06afd1697a0e
Lay Cipriano <webdevlay@gmail.com>
2024-08-07 01:26:43 -0300

    EX05-LISTA03-DONE

---
+++ Lista de Exercícios 03/exercicio_05.py
@@ -0,0 +1,10 @@
+# Crie duas tuplas. Concatene-as para formar uma nova tupla
+
+tupla1 = ('JavaScript, Python, React')
+tupla2 = ('Java, SpringBoot, C++')
+
+concatenar = tupla1 + ", " + tupla2
+
+print(tupla1)
+print(tupla2)
+print(concatenar)
--- Lista de Exercícios 04/exercicio_03.py
+++ Lista de Exercícios 04/exercicio_03.py
@@ -5,38 +5,36 @@ Extra: Crie uma terceira, que é um menu para o usuário escolher a
 opção desejada, onde esse menu chama a função de conversão
 correta.'''
 
-
-# 1. perguntar ao usuário:
-    # 1.1 o valor a ser convertido
-    # 1.2 para qual medida ele deseja converter: Celsius ou Fahrenheit
-# 2. Criar um menu para o usuário
-
-
 # Função Celsius -> Fahrenheit
 def celsiusToFahrenheit(medida):
     resfahrenheit = (medida * 9/5) + 32
     print(f"A conversão de {medida}°C é igual a {resfahrenheit}°F")
     
+# Função Fahrenheit -> Celsius
 def fahrenheitToCelsius(medida):
     resCelsius = (medida - 32) * 5/9
     print(f"A conversão de {medida}°F é igual a {resCelsius}°C")
-    
+
+    # Main - menu do usuário | Processamento das demais funções
 def main():
 # Valor inputado pelo usuário
-    print("_____ CALCULADORA DE CONVERSÃO DE GRAUS _____")
-    print("Qual operação deseja realizar?")
-    print("1. Converter Celsius para Fahrenheit")
-    print("2. Converter Fahrenheit para Celcius")
-    choice = int(input("Digite o número correspondente à sua escolha: "))
-    
-    if choice == 1:
-        medida = float(input("Informe sua temperatura: "))
-        celsiusToFahrenheit(medida)
-    elif choice == 2:
-        medida = float(input("Informe sua temperatura: "))
-        fahrenheitToCelsius(medida)
-    else:
-        print("Opção inválida. Por favor, escolha entre 1 e 2.")
+    try:
+        print("_____ CALCULADORA DE CONVERSÃO DE GRAUS _____")
+        print("Qual operação deseja realizar?")
+        print("1. Converter Celsius para Fahrenheit")
+        print("2. Converter Fahrenheit para Celcius")
+        choice = int(input("Digite o número correspondente à sua escolha: "))
         
+        if choice == 1:
+            medida = float(input("Informe sua temperatura: "))
+            celsiusToFahrenheit(medida)
+        elif choice == 2:
+            medida = float(input("Informe sua temperatura: "))
+            fahrenheitToCelsius(medida)
+        else:
+            print("Opção inválida. Por favor, escolha entre 1 e 2.")
+    except ValueError:
+        print("Informe um valor numérico, por favor!")
+      
 if __name__ == "__main__":
     main()
