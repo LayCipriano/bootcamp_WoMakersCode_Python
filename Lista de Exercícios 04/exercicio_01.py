2fa66dfb8c5675eda13b04e70a106778bbb1bf1a
Lay Cipriano <webdevlay@gmail.com>
2024-08-09 13:22:58 -0300

    EX01-LISTA04-MELHORIAS

--- Lista de Exercícios 04/exercicio_01.py
+++ Lista de Exercícios 04/exercicio_01.py
@@ -1,13 +1,16 @@
 '''Faça um programa, com uma função que necessite de três
 argumentos, e que forneça a soma desses três argumentos.'''
 
-def soma(num1 , num2, num3):
-    return num1 + num2 + num3
+try:
+    def soma(num1 , num2, num3):
+        return num1 + num2 + num3
 
-numero1 = float(input("Informe o primeiro número: "))
-numero2 = float(input("Informe o segundo número: "))
-numero3 = float(input("Informe o terceiro número: "))
+    numero1 = float(input("Informe o primeiro número: "))
+    numero2 = float(input("Informe o segundo número: "))
+    numero3 = float(input("Informe o terceiro número: "))
 
-somar = soma(numero1, numero2, numero3)
+    somar = soma(numero1, numero2, numero3)
 
-print(f"A soma entre os valores informados {numero1}  + {numero2} + {numero3} = {somar}")
+    print(f"A soma entre os valores informados {numero1}  + {numero2} + {numero3} = {somar}")
+except ValueError:
+    print("Informe um número válido")
